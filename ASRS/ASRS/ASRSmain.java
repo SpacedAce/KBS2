8cba95c877e111b09ab9382133c06de196b31cb1
SpacedAce <gerrese@unseen.is>
2016-04-29 13:36:10 +0200

    Connect

---
+++ ASRS/ASRS/ASRSmain.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ASRS;
+
+/**
+ *
+ * @author ace
+ */
+public class ASRSmain {
+    public static void main(String[] arg) throws Exception 
+    {
+        SerialConnection main = new SerialConnection();
+		main.initialize();
+		Thread t=new Thread() {
+			public void run() {
+				//the following line will keep this app alive for 1000 seconds,
+				//waiting for events to occur and responding to them (printing incoming messages to console).
+				try {Thread.sleep(1000000);} catch (InterruptedException ie) {}
+			}
+		};
+		t.start();
+		System.out.println("Started");
+
+    }
+    
+}
