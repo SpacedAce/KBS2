073cd042ca2aab01e606412e4134ffb5f05391bb
aviator1994 <kevinblaakmeer@hotmail.com>
2016-04-29 11:19:43 +0200

    Test

--- TSP/src/me/TSP/Main.java
+++ TSP/src/me/TSP/Main.java
@@ -3,18 +3,6 @@ package me.TSP;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello word");
-<<<<<<< HEAD
-		for(int i = 0; i<10; i++){
-			System.out.println("Ik heb honger");
-		}
-=======
-		System.out.println("BIER");
-		System.out.println("Kutzooi");
-		System.out.println("Kutzooi");
-		System.out.println("Kutzooi");
-		System.out.println("Kutzooi");
-		System.out.println("Kutzooi");
->>>>>>> 253736e46918e7bd28504050779dd21a8bb08e8c
+
     }
 }
