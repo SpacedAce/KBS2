ffd4eed023f975db46f975f74a633d901e5f5b0f
KevinGeubels <kevingeubels@hotmail.com>
2016-04-29 12:40:28 +0200

    helpPanel upgrade :P
    flowyayout toegevoegd aan helpPanel

--- kbs/src/BPP/helpPanel.java
+++ kbs/src/BPP/helpPanel.java
@@ -2,17 +2,19 @@ package BPP;
 
 import java.awt.*;
 import javax.swing.*;
+import javax.swing.border.EmptyBorder;
 
 public class helpPanel extends JFrame{
 	public helpPanel(){
 		setTitle("Help");
-		setSize(500, 800);
+		setSize(600, 800);
+		setLayout(new FlowLayout());
 		
 		ImageIcon image = new ImageIcon(getClass().getResource("helpPanelimg/hoofdscherm.png"));
-		
 		JLabel img = new JLabel();
 		img.setIcon(image);
-		img.setBounds(10, 10, 480, 240);
+//		img.setBounds(10, 10, 480, 240);
+		img.setBorder(new EmptyBorder(10, 50, 10, 50));
 		add(img);
 		
 		setVisible(true);
