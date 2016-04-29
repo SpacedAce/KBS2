253736e46918e7bd28504050779dd21a8bb08e8c
aviator1994 <kevinblaakmeer@hotmail.com>
2016-04-29 10:53:26 +0200

    Edit: TSP/Main.java

--- TSP/src/me/TSP/Main.java
+++ TSP/src/me/TSP/Main.java
@@ -4,14 +4,11 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello word");
-<<<<<<< HEAD
 		System.out.println("BIER");
-=======
 		System.out.println("Kutzooi");
 		System.out.println("Kutzooi");
 		System.out.println("Kutzooi");
 		System.out.println("Kutzooi");
 		System.out.println("Kutzooi");
->>>>>>> 4f4317ba25330c1396e8c767c404f000f24774b6
     }
 }
