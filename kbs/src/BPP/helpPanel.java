f7f27155eb9d8b71296844fb0fa7663ccadbf870
chrisstar321 <s1095375@windesheim.nl>
2016-04-29 10:38:36 +0200

    helpPanel

---
+++ kbs/src/BPP/helpPanel.java
@@ -0,0 +1,12 @@
+package BPP;
+
+import javax.swing.*;
+
+public class helpPanel extends JFrame{
+	public helpPanel(){
+		setTitle("Help");
+		setSize(500, 800);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setVisible(true);
+	}
+}
