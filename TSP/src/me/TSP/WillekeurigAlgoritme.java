a8f67dc48d96354595f888fa6a008200b2832e04
Jeroen Jonkman <jeroenjonkmannetje93@gmail.com>
2016-05-04 19:15:10 +0200

    Update:  Willekeurig algoritme

--- TSP/src/me/TSP/WillekeurigAlgoritme.java
+++ TSP/src/me/TSP/WillekeurigAlgoritme.java
@@ -16,15 +16,11 @@ public class WillekeurigAlgoritme extends Algoritme {
     public void calculate(ArrayList<Vak> locaties) {
         //START TIMER
         long startTime = System.nanoTime() / 1000;
-        ArrayList<Vak> tempZeroPoint = new ArrayList<>();
-        tempZeroPoint.add(locaties.get(0));
-        tempZeroPoint.add(locaties.get(locaties.size()-1));
-        locaties.remove(0);
-        locaties.remove(locaties.size()-1);
+        Vak zeroPoint = new Vak(0, 5, 99);
         super.setBestOrderLocaties(locaties);
         Collections.shuffle(getBestOrderLocaties());
-        getBestOrderLocaties().add(0, tempZeroPoint.get(0));
-        getBestOrderLocaties().add(locaties.size(), tempZeroPoint.get(1));
+        getBestOrderLocaties().add(0, zeroPoint);
+        getBestOrderLocaties().add(locaties.size(), zeroPoint);
         //END TIMER
         long totalTime = System.nanoTime() / 1000 - startTime;
         setTime(totalTime);
