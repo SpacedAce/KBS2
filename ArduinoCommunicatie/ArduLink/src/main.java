93fb62c44cc790815cab2cd28edb1d87dda8ed59
Punkenator <charlenehoogkamer@hotmail.com>
2016-05-09 17:47:38 +0200

    Arduino Communicatie

---
+++ ArduinoCommunicatie/ArduLink/src/main.java
@@ -0,0 +1,16 @@
+import java.awt.EventQueue;
+
+public class main {
+	public static void main(String[] args) {
+        EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                try {
+                    Tutorial1 frame = new Tutorial1();
+                    frame.setVisible(true);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+}
---
+++ ArduinoCommunicatie/ArduinoTest/src/main.java
@@ -0,0 +1,63 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import gnu.io.CommPortIdentifier;
+import gnu.io.SerialPort;
+import gnu.io.SerialPortEvent;
+import gnu.io.SerialPortEventListener;
+import java.util.Enumeration;
+import java.io.IOException;
+
+
+public class main {
+	public static BufferedReader input;
+	public static OutputStream output;
+	private static int c;
+	
+	public static void main(String[] ag) {
+
+
+		try {
+			Setup obj = new Setup();
+			c = 0;
+			obj.initialize();
+			input = Setup.input;
+			output = Setup.output;
+			InputStreamReader Ir = new InputStreamReader(System.in);
+			BufferedReader Br = new BufferedReader(Ir);
+			while (c != 3) {
+				System.out.println("LED ON / OFF MENU.");
+				System.out.println("1.LED ON");
+				System.out.println("2.LED OFF");
+				System.out.print("Enter your choice :");
+				c = Integer.parseInt(Br.readLine());
+
+				switch (c) {
+				case 1:
+					TestClass.writeData("1");
+					break;
+
+				case 2:
+					TestClass.writeData("0");
+					break;
+
+				case 3:
+					System.exit(0);
+				}
+			}
+
+			String inputLine = input.readLine();
+			System.out.println(inputLine);
+
+			obj.close();		
+
+		} catch (Exception e) {
+		}
+
+	}
+	
+	public static void setC(int x){
+		c = x;
+	}
+
+}
