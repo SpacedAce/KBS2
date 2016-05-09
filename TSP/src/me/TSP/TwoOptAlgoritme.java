b69005618d05be8360ce9c940469c341328c7b78
Aviator1994 <kevinblaakmeer@gmail.com>
2016-05-10 01:20:21 +0200

    Edit TwoOptAlgoritme.java Started 2opt setup

--- TSP/src/me/TSP/TwoOptAlgoritme.java
+++ TSP/src/me/TSP/TwoOptAlgoritme.java
@@ -1,5 +1,7 @@
 package me.TSP;
 
+import java.util.ArrayList;
+
 /**
  * Created by Kevin on 29-4-2016.
  */
@@ -8,5 +10,22 @@ public class TwoOptAlgoritme extends Algoritme
     public TwoOptAlgoritme()
     {
         super.setName("Two-Opt Algoritme");
+    }
+
+    @Override
+    public void calculate(ArrayList<Vak> locaties) {
+        ArrayList<Vak> currentSolution = new ArrayList<>();
+        currentSolution.addAll(locaties);
+
+        ArrayList<Vak> newSolution = new ArrayList<>();
+        while(currentSolution.size() > 0) {
+            int bestIndex = 1;
+            for (int i = 1; i < currentSolution.size(); i++)
+            {
+
+            }
+            newSolution.add(currentSolution.get(bestIndex));
+            currentSolution.remove(bestIndex);
+        }
     }
 }
